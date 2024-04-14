84a6e03fdcab65bce531613bb7f9bba512c4bc90
Peter Uadiale <fxavy02@gmail.com>
2024-04-14 12:32:19 +0100

    refactor: add file for project all constatnts

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const modalPortal = document.getElementById(
+  "modal-portal"
+) as HTMLElement;
+
+export const X = "X";
+export const O = "O";
+export const Tie = "Tie";
+
+export const PVP = "PvP";
+export const PVCPU = "PvCPU";
+
+export const EASY = "easy";
+export const NORMAL = "normal";
+export const HARD = "hard";
