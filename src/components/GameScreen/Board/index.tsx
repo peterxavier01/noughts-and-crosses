a80b926f7455f68eaf29a707c4b68e316268ccaf
Peter Uadiale <fxavy02@gmail.com>
2024-04-16 11:43:27 +0100

    feat: add logic to enable cpu play game

--- src/components/GameScreen/Board/index.tsx
+++ src/components/GameScreen/Board/index.tsx
@@ -1,14 +1,27 @@
+import { useEffect } from "react";
+
 import Cell from "../Cell";
 
 import useGame from "../../../store/useGame";
+import useGameMode from "../../../store/useGameMode";
+import useEndGameModal from "../../../store/useEndGameModal";
+
+import { O, PVCPU, X } from "../../../lib/constants";
+import { calculateWinner, cpuMove } from "../../../lib/utils";
 
 import styles from "./index.module.css";
-import { useEffect } from "react";
 
 const Board = () => {
   const currentBoard = useGame((state) => state.currentBoard);
   const winner = useGame((state) => state.winner);
   const updateScore = useGame((state) => state.updateScore);
+  const playerChoice = useGame((state) => state.playerChoice);
+  const turn = useGame((state) => state.turn);
+  const gameDifficulty = useGame((state) => state.gameDifficulty);
+
+  const mode = useGameMode((state) => state.mode);
+
+  const onOpen = useEndGameModal((state) => state.onOpen);
 
   // Update score on game over
   useEffect(() => {
@@ -18,6 +31,37 @@ const Board = () => {
 
     handleScoreUpdate();
   }, [winner, updateScore]);
+
+  // CPU to make move
+  useEffect(() => {
+    const updateBoard = useGame.getState().updateBoard;
+    const setWinner = useGame.getState().setWinner;
+    const toggleCpuTurn = useGame.getState().toggleCpuTurn;
+    const isWinner = calculateWinner(currentBoard);
+
+    if (isWinner) {
+      // Get the winner
+      setWinner(isWinner);
+
+      // // Open modal when the game is over
+      onOpen();
+    } else if (playerChoice.player1 !== turn && mode === PVCPU) {
+      toggleCpuTurn();
+      const CPU = playerChoice.player1 === X ? O : X;
+      const board = [...currentBoard];
+
+      const cpuToMove = cpuMove(board, CPU, gameDifficulty);
+
+      const timer = setTimeout(() => {
+        updateBoard(turn, cpuToMove?.move as number);
+        toggleCpuTurn();
+      }, 500);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [onOpen, currentBoard, mode, playerChoice, winner, turn, gameDifficulty]);
 
   return (
     <div className={styles.container}>
