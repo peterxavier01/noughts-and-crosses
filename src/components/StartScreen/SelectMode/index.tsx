610a02696477170c019814e7e0c170733ad34a0b
peterxavier01 <fxavy02@gmail.com>
2025-08-14 01:48:07 +0100

    refactor: clean up SelectMode component by removing unused multiplayer logic and organizing imports

--- src/components/StartScreen/SelectMode/index.tsx
+++ src/components/StartScreen/SelectMode/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { motion } from "framer-motion";
 
 import { pvcpuVariants, pvpVariants } from "../../../animations";
@@ -7,47 +6,17 @@ import useGame from "../../../store/useGame";
 import useGameMode from "../../../store/useGameMode";
 import useGameScreen from "../../../store/useGameScreen";
 import useDifficultyModal from "../../../store/useDifficultyModal";
-import useMultiPlayer from "../../../store/useMultiPlayer";
+import { PVCPU, PVP, PVPOnline } from "../../../lib/constants";
 
-import { socket } from "../../../lib/socket";
-import { PVCPU, PVP } from "../../../lib/constants";
-
-import styles from "../index.module.css";
+import styles from "./index.module.css";
 
 const SelectMode = () => {
   const startGame = useGame((state) => state.startGame);
 
   const setMode = useGameMode((state) => state.setMode);
-  const screen = useGameScreen((state) => state.screen);
   const setScreen = useGameScreen((state) => state.setScreen);
 
   const onOpen = useDifficultyModal((state) => state.onOpen);
-
-  const roomId = useMultiPlayer((state) => state.roomId);
-  const setRoomId = useMultiPlayer((state) => state.setRoomId);
-  const setPlayers = useMultiPlayer((state) => state.setPlayers);
-
-  // Get the created room id and list of players
-  useEffect(() => {
-    socket.on("roomJoined", (players) => {
-      setPlayers(players);
-    });
-
-    socket.on("roomCreated", (roomId) => {
-      setRoomId(roomId);
-    });
-  }, [setRoomId, setPlayers]);
-
-  // Join room after the room has been created
-  useEffect(() => {
-    if (screen === "play") {
-      roomId && socket.emit("joinRoom", roomId);
-    }
-  }, [roomId, screen]);
-
-  const createRoom = () => {
-    socket.emit("createRoom");
-  };
 
   return (
     <div className={styles.rowThree}>
@@ -65,6 +34,7 @@ const SelectMode = () => {
       >
         New game (vs cpu)
       </motion.button>
+
       <motion.button
         initial="initial"
         animate="animate"
@@ -75,10 +45,23 @@ const SelectMode = () => {
           setScreen("play");
           startGame();
           setMode(PVP);
-          createRoom();
         }}
       >
         new game (vs player)
+      </motion.button>
+
+      <motion.button
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pvcpuVariants}
+        className={styles.btnThree}
+        onClick={() => {
+          setScreen("create-game");
+          setMode(PVPOnline);
+        }}
+      >
+        Online Game (vs player)
       </motion.button>
     </div>
   );
