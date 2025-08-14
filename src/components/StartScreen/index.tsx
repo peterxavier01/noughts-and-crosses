bee242dda869c4f6b13766e10a0df17252aa3df5
peterxavier01 <fxavy02@gmail.com>
2025-08-14 01:47:03 +0100

    refactor: simplify StartScreen component by removing unused code and integrating SelectMark

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -4,52 +4,20 @@ import { motion } from "framer-motion";
 import { logoMarkVariants } from "../../animations";
 
 import SelectMode from "./SelectMode";
+import SelectMark from "./SelectMark";
 
-import IconX from "../../assets/IconX";
 import Logo from "../../assets/logo.svg";
-import IconO from "../../assets/iconO";
 
 import useGame from "../../store/useGame";
-// import useMultiPlayer from "../../store/useMultiPlayer";
-// import useGameScreen from "../../store/useGameScreen";
 
 import styles from "./index.module.css";
 
-// import { socket } from "../../lib/socket";
-import { O, X } from "../../lib/constants";
-
 const StartScreen = () => {
-  const mark = useGame((state) => state.mark);
-  const setMark = useGame((state) => state.setMark);
-  const setPlayerOneChoice = useGame((state) => state.setPlayerOneChoice);
-
-  const handleSelected = (selected: string) => {
-    setMark(selected);
-    setPlayerOneChoice(selected);
-  };
-
   useEffect(() => {
     const resetWinner = useGame.getState().resetWinner;
 
     resetWinner();
   }, []);
-
-  // const screen = useGameScreen((state) => state.screen);
-  // const setScreen = useGameScreen((state) => state.setScreen);
-
-  // // const roomId = useMultiPlayer((state) => state.roomId);
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const roomId = urlParams.get("roomId");
-
-  // useEffect(() => {
-  //   const joinPath = window.location.href.includes("/join");
-
-  //   // // Emit 'joinRoom' event with the room ID to join the room
-  //   if (joinPath && roomId && screen === "start") {
-  //     setScreen("play");
-  //     socket.emit("joinRoom", roomId);
-  //   }
-  // }, [setScreen, roomId, screen]);
 
   return (
     <div className={styles.container}>
@@ -63,32 +31,7 @@ const StartScreen = () => {
         <img src={Logo} alt="noughts and crosses royale logo" />
       </motion.div>
 
-      <motion.div
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        variants={logoMarkVariants}
-        className={styles.rowTwo}
-      >
-        <h1>Pick player 1's mark</h1>
-
-        <div className={styles.selectBox}>
-          <button
-            onClick={() => handleSelected(X)}
-            className={mark === X ? styles.selected : styles.notSelected}
-          >
-            <IconX currentColor="var(--color-dark-navy)" />
-          </button>
-          <button
-            onClick={() => handleSelected(O)}
-            className={mark === O ? styles.selected : styles.notSelected}
-          >
-            <IconO currentColor="var(--color-dark-navy)" />
-          </button>
-        </div>
-
-        <p>remember: x goes first</p>
-      </motion.div>
+      <SelectMark />
 
       <SelectMode />
     </div>
